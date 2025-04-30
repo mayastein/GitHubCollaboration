66920b72030f570a07ae8cf2b3e4a931e80c03aa
Maya Stein <mayarosestein@gmail.com>
2025-04-29 20:13:23 -0400

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Maya Stein on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubCollaborationTests/GitHubCollaborationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationTests.swift
+//  GitHubCollaborationTests
+//
+//  Created by Maya Stein on 4/29/25.
+//
+
+import Testing
+@testable import GitHubCollaboration
+
+struct GitHubCollaborationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
